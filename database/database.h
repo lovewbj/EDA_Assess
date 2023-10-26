add77296ce997097e59921a3a998f2056786d7b1
lovewbj <148614094+lovewbj@users.noreply.github.com>
2023-10-26 23:50:49 +0800

    Update database.h

--- database/database.h
+++ database/database.h
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+struct DieSize
+{
+    int width;
+    int heigth;
+};
+
+struct NumInstances
+{
+    int size;
+    std::vector<std::string> instances;
+};
+
+struct Net
+{
+    int size;
+    std::vector<std::string> pins;
+};
+
+struct NumNets
+{
+    int size;
+    Net* nets[3];
+};
 
