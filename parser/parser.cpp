fe583b774174c52df4228f7adb9adf1aff260d00
lovewbj <148614094+lovewbj@users.noreply.github.com>
2023-10-26 23:23:02 +0800

    Update parser.cpp

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include<algorithm>
+void parseDie(std::ifstream& ifs, DieSize& die);
+void parseInstances(std::ifstream& ifs, NumInstances& instance);
+void parseNumNets(std::ifstream& ifs, NumNets& numnets);
+void parseNets(std::ifstream& ifs, Net& net);
