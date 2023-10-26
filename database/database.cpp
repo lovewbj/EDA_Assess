db793e22616d4bb2a17b5666238402bb6fdafbce
lovewbj <148614094+lovewbj@users.noreply.github.com>
2023-10-26 23:19:23 +0800

    Update database.cpp

---
+++ database/database.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+struct DieSize
+{
+    int width;
+    int heigth;
+};
+
+struct NumInstances
+{
+    int size;
+    std::vector<std::string> instances;
+};
+
+struct Net
+{
+    int size;
+    std::vector<std::string> pins;
+};
+
+struct NumNets
+{
+    int size;
+    Net* nets[3];
+};
