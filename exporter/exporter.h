5719d273b8918a74bb849ab0489d74abc4696922
lovewbj <148614094+lovewbj@users.noreply.github.com>
2023-10-29 01:56:31 +0800

    Update exporter.h

---
+++ exporter/exporter.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include<algorithm>
+include<parder.h>
+include<database.h>
+void ostream(int bestgrathic[3][3]);
