8764d2cbcaa8f6e16f6378ada03a012bfa33108b
lovewbj <148614094+lovewbj@users.noreply.github.com>
2023-10-29 14:57:22 +0800

    Update exporter.cpp

--- exporter/exporter.cpp
+++ exporter/exporter.cpp
@@ -1,7 +1,7 @@
-include<exporter.h>
-void ostream(int bestgrathic[3][3])
+#include "exporter.h"
+void ostream(int bestgrathic[3][3])//把运行结果写入txt文件中，没有进入文件的末尾，是为了写入最后一个结果
 {
-    std:: ofstream ofs("C:/Users/wangbenjie/Desktop/新建 文本文档.txt");
+    std::ofstream ofs("C:/Users/wangbenjie/Desktop/新建 文本文档.txt");
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -16,5 +16,6 @@ void ostream(int bestgrathic[3][3])
                 ofs << "C4" << " " << i << " " << j << std::endl;
         }
     }
+    ofs.close();
     return;
 }
