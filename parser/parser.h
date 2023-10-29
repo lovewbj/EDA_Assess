134f5af11ab33cbc6da61e1eebbe0b8ed86ea866
lovewbj <148614094+lovewbj@users.noreply.github.com>
2023-10-29 14:57:51 +0800

    Update parser.h

--- parser/parser.h
+++ parser/parser.h
@@ -1,11 +1,14 @@
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <vector>
-#include<algorithm>
-#include<database.h>
+#include <algorithm>
+#include "database.h"
+
 void parseDie(std::ifstream& ifs, DieSize& die);
 void parseInstances(std::ifstream& ifs, NumInstances& instance);
-void parseNets(std::ifstream& ifs, Net& net)
+void parseNumNets(std::ifstream& ifs, NumNets& numnets);
+void parseNets(std::ifstream& ifs, Net& net);
 void parseNumNet(Net* net1, Net* net2, Net* net3, NumNets& numnets);
