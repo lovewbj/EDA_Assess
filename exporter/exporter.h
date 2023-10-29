e44d7d604e1b215cd26e8bffebda314c766ff355
lovewbj <148614094+lovewbj@users.noreply.github.com>
2023-10-29 14:56:50 +0800

    Update exporter.h

--- exporter/exporter.h
+++ exporter/exporter.h
@@ -1,9 +1,11 @@
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <vector>
 #include<algorithm>
-include<parder.h>
-include<database.h>
+#include"parser.h"
+#include"database.h"
 void ostream(int bestgrathic[3][3]);
+
