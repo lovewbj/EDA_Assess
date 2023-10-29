5ae4d80d738f782871d7b6af5b628f3e221a723d
lovewbj <148614094+lovewbj@users.noreply.github.com>
2023-10-29 14:59:23 +0800

    Update placement.h

--- placement/placement.h
+++ placement/placement.h
@@ -1,9 +1,10 @@
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <vector>
 #include<algorithm>
-#include<parser.h>
-#include<exporter.h>
-void Calculate(int bestgrathic[3][3], int grathic[3][3], const DieSize & die, int a, NumNets & numnets, int& minsum);
+#include"parser.h"
+#include"exporter.h"
+void Calculate(int bestgrathic[3][3], int grathic[3][3], const DieSize& die, int a, NumNets& numnets, int& minsum);
